a147780e651bd85ce0db43e6bb60d936c6f1dab9
phauer <info@philipphauer.de>
2016-07-29 20:29:10 +0200

    small changes in compare-by-sorting.sh

--- compare-payloads/compare-scripts/compare-by-sorting.sh
+++ compare-payloads/compare-scripts/compare-by-sorting.sh
@@ -4,9 +4,10 @@ if [[ ! -e "payload-output" ]]; then
     mkdir "payload-output"
 fi
 
-http http://localhost:8080/blogposts > payload-output/version1.json
-http http://localhost:8080/blogposts2 > payload-output/version2.json
-# sort all character of the payload alphanumerical. This leads to rubbish. But a rubbish we can compare to determine equality.
+http http://localhost:8080/blogposts --pretty=none > payload-output/version1.json
+http http://localhost:8080/blogposts2 --pretty=none > payload-output/version2.json
+# heuristic: sort all characters of the payload alphanumerical. This leads to rubbish.
+# But we can compare the rubbish to determine equality with a high probability.
 version1Sorted=$(grep -o . "payload-output/version1.json" | sort | tr -d "\n")
 version2Sorted=$(grep -o . "payload-output/version2.json" | sort | tr -d "\n")
 if [ "$version1Sorted" = "$version2Sorted" ]; then
