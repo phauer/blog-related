6cbdb6ee3cb13e88ddafe2365bff76dde2139e92
Philipp Hauer <info@philipphauer.de>
2016-12-14 21:14:53 +0100

    remove jdbc config completely.

--- kotlin-spring-boot-vaadin-scaffolding/src/main/kotlin/de/philipphauer/blog/scaffolding/MyAppSpringConfiguration.kt
+++ kotlin-spring-boot-vaadin-scaffolding/src/main/kotlin/de/philipphauer/blog/scaffolding/MyAppSpringConfiguration.kt
@@ -2,13 +2,9 @@ package de.philipphauer.blog.scaffolding
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder
-import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.beans.factory.InjectionPoint
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.jdbc.core.JdbcTemplate
-import javax.sql.DataSource
-import org.springframework.beans.factory.InjectionPoint
 import org.springframework.context.annotation.Scope
 
 
@@ -20,14 +16,4 @@ open class MyAppSpringConfiguration {
     open fun logger(injectionPoint: InjectionPoint): Logger {
         return LoggerFactory.getLogger(injectionPoint.methodParameter.containingClass)
     }
-
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
-    open fun datasource(): DataSource {
-        return DataSourceBuilder.create().build()
-    }
-
-    @Bean
-    open fun jdbcTemplate(datasource: DataSource) = JdbcTemplate(datasource)
-
 }
