8944fdfbab4f167a0da212c46da25f6f1d9ce540
Philipp Hauer <info@philipphauer.de>
2016-12-18 09:30:54 +0100

    improve YAML example

--- kotlin-spring-boot-vaadin-scaffolding/src/main/kotlin/de/philipphauer/blog/scaffolding/YamlConfigs.kt
+++ kotlin-spring-boot-vaadin-scaffolding/src/main/kotlin/de/philipphauer/blog/scaffolding/YamlConfigs.kt
@@ -7,5 +7,18 @@ import javax.validation.constraints.NotNull
 @Configuration
 @ConfigurationProperties(prefix = "myapp")
 open class MyAppConfig {
-    @NotNull lateinit var myprop: String
+    @NotNull lateinit var requiredProp: String
+    var optionalProp: String? = null
 }
+
+@Configuration
+@ConfigurationProperties(prefix = "myapp.authentication")
+open class AuthenticationConfig {
+    @NotNull lateinit var url: String
+    var credentials: Credentials? = null
+}
+class Credentials{
+    @NotNull lateinit var userName: String
+    @NotNull lateinit var userPassword: String
+}
+
