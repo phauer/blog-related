8aa9cab996119be45c9f69fce263c89200a1c0f6
Philipp Hauer <info@philipphauer.de>
2017-03-25 16:29:52 +0100

    typos and semicolon

--- kotlin-idiomatic/src/main/kotlin/idiomaticKotlin/TopLevelExtensionFunctions.kts
+++ kotlin-idiomatic/src/main/kotlin/idiomaticKotlin/TopLevelExtensionFunctions.kts
@@ -3,15 +3,15 @@ package idiomaticKotlin
 // In Java, you often create static util methods in util classes.
 object StringUtil {
     fun countAmountOfX(string: String): Int{
-        return string.length - string.replace("x", "").length;
+        return string.length - string.replace("x", "").length
     }
 }
 StringUtil.countAmountOfX("xKotlinxFunx") //3
 
-// In Kotlin, remove the uncessary wrapping util class and use top-level functions instead
-// Often, you can addtionally use extension functions, which increases readability ("like a story").
+// In Kotlin, remove the unnecessary wrapping util class and use top-level functions instead
+// Often, you can additionally use extension functions, which increases readability ("like a story").
 fun String.countAmountOfX(): Int {
-    return length - replace("x", "").length;
+    return length - replace("x", "").length
 }
 "xKotlinxFunx".countAmountOfX() //3
 
