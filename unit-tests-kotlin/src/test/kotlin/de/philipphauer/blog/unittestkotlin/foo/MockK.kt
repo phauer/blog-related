d1a8b1b71af8fe7583f9bc61338a02a36dd14a68
Philipp Hauer <info@philipphauer.de>
2018-02-13 18:45:59 +0100

    add usage of MockK and incubation feature of Mockito

---
+++ unit-tests-kotlin/src/test/kotlin/de/philipphauer/blog/unittestkotlin/foo/MockK.kt
@@ -0,0 +1,60 @@
+package de.philipphauer.blog.unittestkotlin.foo
+
+import io.mockk.clearMocks
+import io.mockk.mockk
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.RepeatedTest
+import org.junit.jupiter.api.TestInstance
+
+//with MockK, the mocked class can be final! no changes required!
+
+// Do:
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class DesignControllerTest_MockK {
+
+    private val dao: DesignDAO = mockk()
+    private val mapper: DesignMapper = mockk()
+    private val controller = DesignController(dao, mapper)
+
+    @BeforeEach
+    fun init() {
+        clearMocks(dao, mapper)
+    }
+
+    // takes 250 ms
+    @RepeatedTest(300)
+    fun foo() {
+        controller.doSomething()
+    }
+}
+
+//Don't
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class DesignControllerTest_RecreatingMocks_MockK {
+
+    private lateinit var dao: DesignDAO
+    private lateinit var mapper: DesignMapper
+    private lateinit var controller: DesignController
+
+    @BeforeEach
+    fun init() {
+        dao = mockk()
+        mapper = mockk()
+        controller = DesignController(dao, mapper)
+    }
+
+    // takes 2 s! (mockk is even slower (0,5 s) than mockito-kotlin)
+    // but this approach is deprecated anyway.
+    @RepeatedTest(300)
+    fun foo() {
+        controller.doSomething()
+    }
+}
+
+class DesignDAO
+class DesignMapper
+class DesignController(val dao: DesignDAO, val mapper: DesignMapper) {
+    fun doSomething() {
+
+    }
+}
