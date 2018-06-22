d3c394ea28af4a8f4a78f4ab1f2ee5f8af7498d8
Philipp Hauer <info@philipphauer.de>
2018-06-22 18:07:42 +0200

    clean up imports

--- cairosvg-on-alpine/src/svg-converter-service.py
+++ cairosvg-on-alpine/src/svg-converter-service.py
@@ -1,6 +1,5 @@
-import random
 import cairosvg
-from flask import Flask, Response, request
+from flask import Flask, Response
 
 app = Flask(__name__)
 
@@ -8,7 +7,6 @@ app = Flask(__name__)
 def convert_image():
     png_data = cairosvg.svg2png(url="circle.svg", parent_width=300, parent_height=300)
     return Response(png_data, mimetype='image/png')
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
