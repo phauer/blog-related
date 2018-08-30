fb6156edddb4c0a068d7c9796deaf7baa55bfa58
Philipp Hauer <info@philipphauer.de>
2018-08-30 17:06:16 +0200

    add introduction example

---
+++ unit-tests-kotlin/src/test/kotlin/de/philipphauer/blog/unittestkotlin/IntroductionExample.kt
@@ -0,0 +1,50 @@
+package de.philipphauer.blog.unittestkotlin
+
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.reset
+import com.nhaarman.mockito_kotlin.whenever
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.BeforeClass
+import org.junit.Test
+import org.mockito.Mockito.*
+
+class UserControllerTest {
+    companion object {
+        @JvmStatic private lateinit var controller: UserController
+        @JvmStatic private lateinit var repo: UserRepository
+        @BeforeClass @JvmStatic fun initialize() {
+            repo = mock()
+            controller = UserController(repo)
+        }
+    }
+    @Test
+    fun findUser_UserFoundAndHasCorrectValues() {
+        `when`((repo.findUser(1))).thenReturn(User(1, "Peter"))
+        val user = controller.getUser(1)
+        assertEquals(user?.name, "Peter")
+    }
+    @Before
+    fun clear(){
+        reset(repo)
+    }
+}
+
+open class UserRepository {
+    open fun findUser(id: Int): User? {
+        return User(id, "Peter")
+    }
+}
+
+class UserController(
+    val repo: UserRepository
+) {
+    fun getUser(id: Int): User? {
+        return repo.findUser(id)
+    }
+}
+
+data class User(
+    val id: Int,
+    val name: String
+)
