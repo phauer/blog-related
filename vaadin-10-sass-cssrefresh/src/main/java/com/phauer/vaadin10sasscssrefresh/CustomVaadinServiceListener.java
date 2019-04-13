ed59e8e08da20ccd8dfff3948fb032c397098f24
Philipp Hauer <info@philipphauer.de>
2019-04-13 14:16:16 +0200

    small changes

--- vaadin-10-sass-cssrefresh/src/main/java/com/phauer/vaadin10sasscssrefresh/CustomVaadinServiceListener.java
+++ vaadin-10-sass-cssrefresh/src/main/java/com/phauer/vaadin10sasscssrefresh/CustomVaadinServiceListener.java
@@ -4,27 +4,23 @@ import com.vaadin.flow.server.BootstrapListener;
 import com.vaadin.flow.server.BootstrapPageResponse;
 import com.vaadin.flow.server.ServiceInitEvent;
 import com.vaadin.flow.server.VaadinServiceInitListener;
-import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomVaadinServiceListener implements VaadinServiceInitListener {
-
     @Override
     public void serviceInit(ServiceInitEvent event) {
         if (!event.getSource().getDeploymentConfiguration().isProductionMode()) {
             event.addBootstrapListener(new CustomBootstrapListener());
         }
     }
-
     static class CustomBootstrapListener implements BootstrapListener {
-
         @Override
         public void modifyBootstrapPage(BootstrapPageResponse response) {
-            Document doc = response.getDocument();
-            doc.head().append("<script type=\"text/javascript\" src=\"/js/cssrefresh.js\"></script>");
+            Element head = response.getDocument().head();
+            head.append("<script type=\"text/javascript\" src=\"/js/cssrefresh.js\"></script>");
         }
-
     }
 }
 
