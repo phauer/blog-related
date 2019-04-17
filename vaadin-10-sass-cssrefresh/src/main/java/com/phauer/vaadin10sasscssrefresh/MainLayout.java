e2a84b1a8e71392482f501f30acfbb7abe72b22b
Philipp Hauer <info@philipphauer.de>
2019-04-17 21:00:08 +0200

    use a more production-mode-friendly way of addressing main.css

--- vaadin-10-sass-cssrefresh/src/main/java/com/phauer/vaadin10sasscssrefresh/MainLayout.java
+++ vaadin-10-sass-cssrefresh/src/main/java/com/phauer/vaadin10sasscssrefresh/MainLayout.java
@@ -7,7 +7,7 @@ import com.vaadin.flow.component.page.Viewport;
 import com.vaadin.flow.router.RouterLayout;
 
 @Push
-@StyleSheet("frontend://styles/main.css")
+@StyleSheet("/frontend/styles/main.css")
 @Viewport("width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes")
 public class MainLayout extends Div implements RouterLayout {
     public MainLayout() {
