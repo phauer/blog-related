42212198000d8c77ebde4e2856d891bb3a7e8eac
Philipp Hauer <info@philipphauer.de>
2019-04-28 20:23:03 +0200

    mongodb stuff

---
+++ mongodb-practice/connection-strings.sh
@@ -0,0 +1,7 @@
+mongo --host db1.domain.com --port 27017 -u writeUser -p CPy8f82cb productService
+mongo --host db1.domain.com --port 27017 -u readUser -p qBr7bEJSm productService
+mongo --host db2.domain.com --port 27018 -u writeUser -p oFfQdzkKR checkout
+mongo --host db2.domain.com --port 27018 -u readUser -p ehhhTbmeT checkout
+mongo --host db3.domain.com --port 27019 -u writeUser -p LkFnSmNYb shop
+mongo --host db3.domain.com --port 27019 -u readUser -p 4aYLLcgn8 shop
+mongo --host localhost --port 27017
