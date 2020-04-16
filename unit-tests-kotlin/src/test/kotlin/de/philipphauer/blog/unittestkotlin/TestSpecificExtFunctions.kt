36564f30ef8454978cabe5bd66958a3e65edf9d3
Philipp Hauer <info@philipphauer.de>
2020-04-16 20:46:28 +0200

    add kotest's equivalent to assertj's isCloseTo()

---
+++ unit-tests-kotlin/src/test/kotlin/de/philipphauer/blog/unittestkotlin/TestSpecificExtFunctions.kt
@@ -0,0 +1,35 @@
+package de.philipphauer.blog.unittestkotlin
+
+import io.kotest.matchers.floats.plusOrMinus
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+
+class TestSpecificExtFunctions {
+
+    // Don't
+    @Test
+    fun bla() {
+        val taxRate1 = 0.3f
+        val taxRate2 = 0.2f
+        val taxRate3 = 0.5f
+
+        taxRate1 shouldBe 0.3f.plusOrMinus(0.001f)
+        taxRate2 shouldBe 0.2f.plusOrMinus(0.001f)
+        taxRate3 shouldBe 0.5f.plusOrMinus(0.001f)
+    }
+
+    // Do
+    @Test
+    fun bla2() {
+        val taxRate1 = 0.3f
+        val taxRate2 = 0.2f
+        val taxRate3 = 0.5f
+
+        taxRate1 shouldBeCloseTo 0.3f
+        taxRate2 shouldBeCloseTo 0.2f
+        taxRate3 shouldBeCloseTo 0.5f
+    }
+
+    private infix fun Float.shouldBeCloseTo(expected: Float) = this shouldBe expected.plusOrMinus(0.001f)
+}
